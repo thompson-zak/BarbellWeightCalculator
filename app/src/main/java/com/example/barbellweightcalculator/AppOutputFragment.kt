3c5470c2e1736e8e77a9b10cf82b8b7223be7c08
Zak Thompson <zakthompson97@gmail.com>
2022-07-15 05:17:37 -0500

    Add safeArgs and implement initial data pass from input to output fragments

--- app/src/main/java/com/example/barbellweightcalculator/AppOutputFragment.kt
+++ app/src/main/java/com/example/barbellweightcalculator/AppOutputFragment.kt
@@ -33,7 +33,7 @@ class AppOutputFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_AppOutput_to_UserInput)
         }
     }
 
--- app/src/main/java/com/example/barbellweightcalculator/UserInputFragment.kt
+++ app/src/main/java/com/example/barbellweightcalculator/UserInputFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.barbellweightcalculator.databinding.UserInputFragmentBinding
 
+
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A [Fragment] that takes a weight and type as input
+ * Then converts it and navigates to the display [Fragment] for the conversion
  */
 class UserInputFragment : Fragment() {
 
@@ -21,19 +26,41 @@ class UserInputFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+            savedInstanceState: Bundle?): View? {
+
+        val view: View = inflater.inflate(R.layout.user_input_fragment, container, false)
+        val radioGroup = view.findViewById(R.id.weight_selection_radio_original) as RadioGroup
+        val conversionLabel = view.findViewById(R.id.conversion_label) as TextView
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val rb = group.findViewById(checkedId) as RadioButton
+            val rbText = rb.text as String
+            // If user selects kg as input, notify them they will be converting to lbs - as well as the reverse
+            if(rbText.contains("kg"))
+                conversionLabel.setText(R.string.converting_to_lbs)
+            else
+                conversionLabel.setText(R.string.converting_to_kgs)
+        }
 
         _binding = UserInputFragmentBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            // Get weight integer and unit then pass to next fragment as args
+            val conversionWeightTextView = view.findViewById(R.id.input_weight_form) as TextView
+            val conversionWeight = Integer.parseInt(conversionWeightTextView.text.toString())
+
+            val radioGroup = view.findViewById(R.id.weight_selection_radio_original) as RadioGroup
+            val selectedRadioBtn = view.findViewById(radioGroup.checkedRadioButtonId) as RadioButton
+            val unitOfWeight = selectedRadioBtn.text.toString()
+
+            val bundle = Bundle()
+            bundle.putInt("weight", conversionWeight)
+            bundle.putString("unit", unitOfWeight)
+            findNavController().navigate(R.id.action_UserInput_to_AppOutput, bundle)
         }
     }
 
