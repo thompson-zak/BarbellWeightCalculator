8b88e43dff51dd51cdc2c206feb347598b81bacc
Zak Thompson <zakthompson97@gmail.com>
2022-07-24 12:31:35 -0500

    Visual code mostly done. Issues with visibility for barbell and plates.

--- app/src/main/java/com/example/barbellweightcalculator/AppOutputFragment.kt
+++ app/src/main/java/com/example/barbellweightcalculator/AppOutputFragment.kt
@@ -1,14 +1,15 @@
 package com.example.barbellweightcalculator
 
 import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.barbellweightcalculator.databinding.AppOutputFragmentBinding
 import kotlin.math.floor
 import kotlin.math.roundToInt
@@ -58,43 +59,118 @@ class AppOutputFragment : Fragment() {
 
             val convertedWeightAndUnit : TextView = view.findViewById(R.id.converted_weight_and_unit) as TextView
             convertedWeightAndUnit.text = "$weight $unit --> $convertedWeight $convertedUnit"
-            convertedWeightAndUnit.invalidate()
-            convertedWeightAndUnit.requestLayout()
 
-            val barbellMap : HashMap<String, Int> = HashMap()
+            // Barbell list is an ordered list of plate amounts to be used 25kg -> 1.25kg
+            val barbellList : ArrayList<Int> = ArrayList()
             val sharedPreferences : SharedPreferences = requireContext().getSharedPreferences("Barbell", 0)
             // Initially remove weight of bar from converted weight, then halve it since this is per side
             var tempWeight = (convertedWeight - 20)/2.0
+
             if (sharedPreferences.getBoolean("25kg", true)) {
-                barbellMap.put("25kg", floor(tempWeight/25.0).toInt())
+                barbellList.add(floor(tempWeight/25.0).toInt())
                 tempWeight %= 25
-            }
-            if (sharedPreferences.getBoolean("20kg", true)) {
-                barbellMap.put("20kg", floor(tempWeight/20.0).toInt())
-                tempWeight %= 20
-            }
-            if (sharedPreferences.getBoolean("15kg", true)) {
-                barbellMap.put("15kg", floor(tempWeight/15.0).toInt())
-                tempWeight %= 15
-            }
-            if (sharedPreferences.getBoolean("10kg", true)) {
-                barbellMap.put("10kg", floor(tempWeight/10.0).toInt())
-                tempWeight %= 10
-            }
-            if (sharedPreferences.getBoolean("5kg", true)) {
-                barbellMap.put("5kg", floor(tempWeight/5.0).toInt())
-                tempWeight %= 5
-            }
-            if (sharedPreferences.getBoolean("2_5kg", true)) {
-                barbellMap.put("2_5kg", floor(tempWeight/2.5).toInt())
-                tempWeight %= 2.5
-            }
-            if (sharedPreferences.getBoolean("1_2_5kg", true)) {
-                barbellMap.put("1_2_5kg", floor(tempWeight/1.25).toInt())
-                tempWeight %= 1.25
+            } else {
+                barbellList.add(0)
             }
 
-            Log.v("AppOutput", barbellMap.toString())
+            if (sharedPreferences.getBoolean("20kg", true)) {
+                barbellList.add(floor(tempWeight/20.0).toInt())
+                tempWeight %= 20
+            } else {
+                barbellList.add(0)
+            }
+
+            if (sharedPreferences.getBoolean("15kg", true)) {
+                barbellList.add(floor(tempWeight/15.0).toInt())
+                tempWeight %= 15
+            } else {
+                barbellList.add(0)
+            }
+
+            if (sharedPreferences.getBoolean("10kg", true)) {
+                barbellList.add(floor(tempWeight/10.0).toInt())
+                tempWeight %= 10
+            } else {
+                barbellList.add(0)
+            }
+
+            if (sharedPreferences.getBoolean("5kg", true)) {
+                barbellList.add(floor(tempWeight/5.0).toInt())
+                tempWeight %= 5
+            } else {
+                barbellList.add(0)
+            }
+
+            if (sharedPreferences.getBoolean("2_5kg", true)) {
+                barbellList.add(floor(tempWeight/2.5).toInt())
+                tempWeight %= 2.5
+            } else {
+                barbellList.add(0)
+            }
+
+            if (sharedPreferences.getBoolean("1_2_5kg", true)) {
+                barbellList.add(floor(tempWeight/1.25).toInt())
+                tempWeight %= 1.25
+            } else {
+                barbellList.add(0)
+            }
+
+            Log.v("AppOutput", barbellList.toString())
+
+            var plateIndex = 1
+            for(i in 0..6) {
+                val numPlates = barbellList[i]
+                val plateColor = getPlateColor(i)
+                val plateHeight = getPlateHeight(i)
+                val layoutParams = ConstraintLayout.LayoutParams(8, plateHeight)
+                // Plates will only get smaller than max 125, so take the difference and add to default 68 margin
+                layoutParams.topMargin = 68 + (125-plateHeight)
+                for(j in plateIndex until (plateIndex+numPlates)) {
+                    val plateId = "plate_$plateIndex"
+                    val plateViewId = resources.getIdentifier(plateId, "id", requireContext().packageName)
+                    val plateView = view.findViewById(plateViewId) as View
+
+                    requireActivity().runOnUiThread {
+                        plateView.layoutParams = layoutParams
+                        plateView.setBackgroundColor(Color.parseColor(plateColor))
+                        plateView.visibility = View.VISIBLE
+                        plateView.postInvalidate()
+                    }
+                }
+                plateIndex += numPlates
+            }
+        }
+    }
+
+    // Returns expected color based on plate list index
+    private fun getPlateColor(plateIndex: Int) : String {
+        return when (plateIndex) {
+            0 -> "#FF0000" //25kg
+            1 -> "#4682B4" //20kg
+            2 -> "#FFC300" //15kg
+            3 -> "#008000" //10kg
+            4 -> "#000000" //5kg
+            5 -> "#000000" //2.5kg
+            6 -> "#000000" //1.25kg
+            else -> {
+                "#FF0000"
+            }
+        }
+    }
+
+    // Returns expected height based on plate list index
+    private fun getPlateHeight(plateIndex: Int) : Int {
+        return when (plateIndex) {
+            0 -> 125 //25kg
+            1 -> 125 //20kg
+            2 -> 100 //15kg
+            3 -> 75  //10kg
+            4 -> 45  //5kg
+            5 -> 30  //2.5kg
+            6 -> 15  //1.25kg
+            else -> {
+                125
+            }
         }
     }
 
