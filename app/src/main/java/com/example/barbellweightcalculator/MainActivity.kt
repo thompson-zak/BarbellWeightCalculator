bf09da1a064b523ee21c6de77ae4813aebd19246
Zak Thompson <zakthompson97@gmail.com>
2022-07-23 21:50:04 -0500

    Removed unused style elements. Navigation to basic settings fragment works from user input screen.

--- app/src/main/java/com/example/barbellweightcalculator/MainActivity.kt
+++ app/src/main/java/com/example/barbellweightcalculator/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.barbellweightcalculator
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -27,11 +26,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -45,7 +39,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_UserInput_to_Settings)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
---
+++ app/src/main/java/com/example/barbellweightcalculator/SettingsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.barbellweightcalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.barbellweightcalculator.databinding.AppOutputFragmentBinding
+import com.example.barbellweightcalculator.databinding.SettingsFragmentBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment() {
+
+    private var _binding: SettingsFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = SettingsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
